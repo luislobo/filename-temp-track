2bbfcc94d29ff98736b388b0112ff2137a513b51
Mudrekh Goderya <mudrekh.goderya@petrocloud.com>
2019-12-23 13:15:19 -0600

    Version 2.0

--- test/test.js
+++ test/test.js
@@ -1,73 +1,55 @@
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var fs = require('fs');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const fs = require('fs');
 chai.use(chaiAsPromised);
 chai.should();
-var ft = require('../lib/index');
+
+const Promise = require('bluebird');
+
+const ft = require('../lib/index');
 
 // according to your preference of assertion style
 
-describe('Track a file', function () {
+describe('Track a file', () => {
 
-  beforeEach(function () {
-    ft.cleanupAllSync();
+  beforeEach(() => ft.cleanupAllSync());
+
+  it('should return a full path name', () => ft.track('myfile.txt')
+    .then(pathName => pathName)
+    .should.eventually.contain('myfile.txt'));
+
+  it('should be able to track the same file more than one time', () => ft.track('myfile.txt')
+    .then(() => ft.track('myfile.txt'))
+    .should.eventually.contain('myfile.txt'));
+
+  it('should clear a filename', async () => {
+    const pathname = await ft.track('another.txt');
+    // creates an empty file
+    await ft.cleanupFile(pathname);
+    ft.getFilesBeingTracked().should.be.an('array').and.be.empty;
   });
 
-  it('should return a full path name', function () {
-    return ft.track('myfile.txt')
-      .then(function (pathName) {
-        return pathName;
-      })
-      .should.eventually.contain('myfile.txt');
-  });
-
-  it('should be able to track the same file more than one time', function () {
-    return ft.track('myfile.txt')
-      .then(function () {
-        return ft.track('myfile.txt');
-      })
-      .should.eventually.contain('myfile.txt');
-  });
-
-  it('should clear a filename', function () {
-    return ft.track('another.txt')
-      .then(function (pathName) {
-        // creates an empty file
-        return ft.cleanupFile(pathName)
-          .then(function () {
-            ft.getFilesBeingTracked().should.be.an('array').and.be.empty;
-            return true;
-          });
-      });
-  });
-
-  it('should be able to track several files and clear them all', function () {
-    return Promise.all([
+  it('should be able to track several files and clear them all', async () => {
+    const result = await Promise.all([
       ft.track('1.txt'),
       ft.track('2.txt'),
       ft.track('3.txt')
-    ])
-      .then(function (result) {
-        result.should.be.an('array').and.have.lengthOf(3);
-        return ft.cleanupAll()
-          .then(function () {
-            ft.getFilesBeingTracked().should.be.an('array').and.be.empty;
-            return true;
-          });
-      });
+    ]);
+
+    result.should.be.an('array').and.have.lengthOf(3);
+    await ft.cleanupAll();
+    ft.getFilesBeingTracked().should.be.an('array').and.be.empty;
   });
 
-  it('should track a file, wait for it\'s timeout, and the file should be gone', function () {
-    return ft.track({filename: '3.txt', cleanupTimeout: 100})
-      .then(function (pathName) {
-        // create the file
-        fs.closeSync(fs.openSync(pathName, 'w'));
-        fs.existsSync(pathName).should.be.true;
-        return pathName;
-      })
-      .delay(150)
-      .then(function (pathName) {
-        fs.existsSync(pathName).should.be.false;
-      });
+  it('should track a file, wait for it\'s timeout, and the file should be gone', async () => {
+    const pathname = await ft.track({
+      filename: '3.txt',
+      cleanupTimeout: 100
+    });
+    fs.closeSync(fs.openSync(pathname, 'w'));
+    fs.existsSync(pathname).should.be.true;
+
+    await Promise.delay(150);
+    fs.existsSync(pathname).should.be.false;
   });
 });
